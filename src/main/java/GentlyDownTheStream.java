53c3dc957c2db0453c38753a56e82bf1a4700eab
nathunn <nathandelongyt@gmail.com>
2022-03-30 11:53:35 -0700

    Initial commit

---
+++ src/main/java/GentlyDownTheStream.java
@@ -0,0 +1,112 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * These coding kata on the Stream API can all be completed with a single return line.
+ */
+public class GentlyDownTheStream {
+
+    protected List<String> fruits;
+    protected List<String> veggies;
+    protected List<Integer> integerValues;
+
+    public GentlyDownTheStream() {
+        fruits = Arrays.asList("Apple", "Banana", "Orange", "Pear", "Peach", "Tomato");
+        veggies = Arrays.asList("Corn", "Potato", "Carrot", "Pea", "Tomato");
+        integerValues = new Random().ints(0, 1001)
+                .boxed()
+                .limit(1000)
+                .collect(Collectors.toList());
+    }
+
+    // TODO - return a list of sorted fruits based on the fruits list
+    public List<String> sortedFruits() {
+        return fruits.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        //DONE
+    }
+
+    // TODO - return a list of sorted fruits with all fruits filtered out that start with "A"
+    public List<String> sortedFruitsException() {
+        return fruits.stream()
+                .filter(fruits -> !fruits.startsWith("A"))
+                .sorted()
+                .collect(Collectors.toList());
+        //DONE
+    }
+
+    // TODO - return a list with 6the first 2 elements of a sorted list of fruits
+    public List<String> sortedFruitsFirstTwo() {
+        return fruits.stream()
+                .limit(2)
+                .sorted()
+                .collect(Collectors.toList());
+        //DONE
+    }
+
+    // TODO - return a comma separated String of sorted fruits
+    public String commaSeparatedListOfFruits() {
+        return fruits.stream()
+                .collect(Collectors.joining(","));
+        //DONE
+    }
+
+    // TODO - return a list of veggies that are sorted in reverse (descending) order
+    public List<String> reverseSortedVeggies() {
+        return veggies.stream()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
+        //DONE
+    }
+
+    // TODO - return a list of veggies that are sorted in reverse order, and all in upper case
+    public List<String> reverseSortedVeggiesInUpperCase() {
+        return veggies.stream()
+                .map(String::toUpperCase)
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
+        //DONE
+    }
+
+    // TODO - return a list of the top 10 values in the list of random integers
+    public List<Integer> topTen() {
+        return integerValues.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(10)
+                .collect(Collectors.toList());
+
+        //DONE
+    }
+
+    // TODO - return a list of the top 10 unique values in the list of random integers
+    public List<Integer> topTenUnique() {
+        return integerValues.stream()
+                .sorted(Comparator.reverseOrder())
+                .distinct()
+                .limit(10)
+                .collect(Collectors.toList());
+        //DONE
+    }
+
+    // TODO - return a list of the top 10 unique values in the list of random integers that are odd
+    public List<Integer> topTenUniqueOdd() {
+        return integerValues.stream()
+                .filter(integerValues -> integerValues % 2 != 0)
+                .sorted(Comparator.reverseOrder())
+                .distinct()
+                .limit(10)
+                .collect(Collectors.toList());
+        //DONE
+    }
+
+    // TODO - return a single value that represents the average of all of your random numbers
+    public Double average() {
+       return integerValues.stream()
+                .mapToInt(integerValues -> integerValues)
+                .limit(10)
+                .average()
+                .orElse(0.0);
+        //DONE
+    }
+}
